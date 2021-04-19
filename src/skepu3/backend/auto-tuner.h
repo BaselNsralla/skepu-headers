db131df0fb023a34e8a9d7ed845a00d510a96ace
Lized <basns594@student.liu.se>
2021-04-19 08:24:39 +0200

    Initial sampling method

--- src/skepu3/backend/auto-tuner.h
+++ src/skepu3/backend/auto-tuner.h
@@ -20,41 +20,30 @@ namespace autotuner
         }
     };
 
+    template<typename T>
+    void print_impl(skepu::Matrix<T>& vec) {
+        std::cout << "Matrix Print" << std::endl;
+    }
 
-    // template<typename Container, typename T, size_t Is, size_t Size>
-    // struct sample {
-    //     template<typename Tuple>
-    //     static void sample_imp(Tuple& tuple) {
-    //         std::cout << "None" << std::endl;
-    //     }
-    // };
-
-    // template<typename T, size_t Index, size_t Size>
-    // struct sample<skepu::Matrix<T>, T, Index, Size> {
-    //     template<typename Tuple>
-    //     static void sample_imp(Tuple& tuple) {
-    //         std::cout << "Matrix" << std::endl;
-    //     }
-    // };
-
-    // template<typename T, size_t Index, size_t Size>
-    // struct sample<skepu::Vector<T>, T, Index, Size> {
-    //     template<typename Tuple>
-    //     static void sample_imp(Tuple& tuple) {
-    //         std::cout << "Vector" << std::endl;
-    //     }
-    // };
+    template<typename T>
+    void print_impl(skepu::Vector<T>& vec) {
+        std::cout << "Vector Print" << std::endl;
+        for(auto& val: vec) {
+            std::cout << val << std::endl;
+        }
+    }
 
 
 
     template<typename T>
     void sample_impl(skepu::Matrix<T>& vec) {
-        std::cout << "Vector" << std::endl;
+        std::cout << "Matrix Sample" << std::endl;
     }
 
     template<typename T>
     void sample_impl(skepu::Vector<T>& vec) {
-        std::cout << "Vector" << std::endl;
+        std::cout << "Vector Sample" << std::endl;
+        vec.init(300, T{}); // Eller utan T{}
     }
     
     template<size_t Index>
@@ -65,11 +54,16 @@ namespace autotuner
         //     sample_impl(std::get<Index>(tup), std::forward<Extra>(extras)...);
         //     foreach<Index-1>::apply(tup, std::forward<Extra>(extras)...);
         // }
+        template<typename Tuple>
+        static void sample(Tuple& tup) {
+            sample_impl(std::get<Index - 1>(tup));
+            foreach<Index-1>::sample(tup);
+        }
 
         template<typename Tuple>
-        static void apply(Tuple& tup) {
-            sample_impl(std::get<Index - 1>(tup));
-            foreach<Index-1>::apply(tup);
+        static void print(Tuple& tup) {
+            print_impl(std::get<Index - 1>(tup));
+            foreach<Index-1>::print(tup);
         }
     };
 
@@ -78,35 +72,30 @@ namespace autotuner
     {
         // template<typename Tuple, typename... Extra>
         // static void apply(Tuple& tup, Extra&&... extras) {}
-
         template<typename Tuple>
-        static void apply(Tuple& tup) {}
+        static void sample(Tuple& tup) {}
+        template<typename Tuple>
+        static void print(Tuple& tup) {}
     };
     
 
     template<typename Tuple, size_t... Idx>
-    void sample(Tuple& tup, pack_indices<Idx...> ) {
+    void sample_all(Tuple& tup, pack_indices<Idx...> ) {
         //size_t l = { (sample_impl(std::get<Idx>(tup)), 0)..., 0 };
         std::initializer_list<size_t> indicies = {Idx...};
         //auto m = std::max(indicies, [](size_t const& a, size_t const& b) { return a < b; });
         std::cout << std::tuple_size<Tuple>::value - 1 << std::endl;
-        foreach< std::template tuple_size<Tuple>::value >::apply(tup);
+        foreach< std::template tuple_size<Tuple>::value >::sample(tup);
     }
 
-    
-    // template<size_t... Ns, typename std::enable_if<sizeof...(Ns) == 0>::type>
-    // void print_them() {}
-
-    // template<size_t a>
-    // void print_them() {
-    //     std::cout << a << std::endl;
-    // }
-
-    // template<size_t a, size_t... Ns, typename std::enable_if<sizeof...(Ns) != 0>::type>
-    // void print_them() {
-    //     std::cout << a << std::endl;
-    //     print_them<Ns...>();
-    // }
+    template<typename Tuple, size_t... Idx>
+    void print_all(Tuple& tup, pack_indices<Idx...> ) {
+        //size_t l = { (sample_impl(std::get<Idx>(tup)), 0)..., 0 };
+        std::initializer_list<size_t> indicies = {Idx...};
+        //auto m = std::max(indicies, [](size_t const& a, size_t const& b) { return a < b; });
+        std::cout << std::tuple_size<Tuple>::value - 1 << std::endl;
+        foreach< std::template tuple_size<Tuple>::value >::print(tup);
+    }
 
     template<bool PrefersMatrix, typename T> 
     using PreferedContainer = typename std::conditional<PrefersMatrix, skepu::Matrix<T>, skepu::Vector<T>>::type;
@@ -116,16 +105,12 @@ namespace autotuner
                                     typename add_container_layer<skepu::template Matrix, TupleType>::type,
                                     typename add_container_layer<skepu::template Vector, TupleType>::type>::type;
 
-    template<typename Skeleton, size_t... OI, size_t... EI, size_t... AI, size_t... CI>
-    void tune(Skeleton& skeleton, pack_indices<OI...> oi, pack_indices<EI...> ei, pack_indices<AI...> ai, pack_indices<CI...> ci) 
+    template<typename Skeleton, size_t... OI, size_t... EI, size_t... CI, size_t... UI>
+    void tune(Skeleton& skeleton, pack_indices<OI...> oi, pack_indices<EI...> ei, pack_indices<CI...> ci, pack_indices<UI...> ui) 
     {
         // ta ut varje parameter frå call args och använd dem i implementationen gör en benchmark och
         // spara undan, kanske låta resten av programmet köra medan vi håller på här?        
         std::cout << "============" << std::endl;
-        // std::cout << OI... << std::endl;
-        // std::cout << EI... << std::endl;
-        // std::cout << AI... << std::endl;
-        // std::cout << CI... << std::endl;
         //print_them<OI...>();
         //print_them<EI...>();
         //print_them<AI...>();
@@ -133,9 +118,20 @@ namespace autotuner
         static constexpr bool pm = Skeleton::prefers_matrix;
 
         PreferedContainer<pm, int> hi{};
+        ArgContainerTup<pm, typename Skeleton::ResultArg> resultArg;
         ArgContainerTup<pm, typename Skeleton::ElwiseArgs> elwiseArg;
-        sample(elwiseArg, ei);
+        ArgContainerTup<pm, typename Skeleton::ContainerArgs> containerArg;
+        ArgContainerTup<pm, typename Skeleton::UniformArgs> uniArg;
+        sample_all(resultArg, oi);
+        sample_all(elwiseArg, ei);
+        sample_all(containerArg, ci);
+        sample_all(uniArg, ui);
+        //sample_all(resultArg, oi);
+        //sample_all(elwiseArg, ei);
+        //sample_all(containerArg, ci);
+        //sample_all(uniArg, ui);
         print<EI...>::_print();
+        std::cout << "============" << std::endl;
     }
 
     template<typename Skeleton>
