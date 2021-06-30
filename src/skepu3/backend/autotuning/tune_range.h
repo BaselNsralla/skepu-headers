9bc948639e57ee49e79e9fdb060126811fb8bc1b
Lized <basns594@student.liu.se>
2021-06-30 10:37:21 +0200

    Tuning values in header

--- src/skepu3/backend/autotuning/tune_range.h
+++ src/skepu3/backend/autotuning/tune_range.h
@@ -1,4 +1,10 @@
 #pragma once
 
-#define MULTI_DIM  15
-#define SINGLE_DIM 18
+/*
+#define MULTI_DIM     15u
+#define SINGLE_DIM    18u
+#define TUNE_CAPACITY 24u
+*/
+#define MULTI_DIM     std::size_t(9u)
+#define SINGLE_DIM    std::size_t(12u)
+#define TUNE_CAPACITY std::size_t(24u)
